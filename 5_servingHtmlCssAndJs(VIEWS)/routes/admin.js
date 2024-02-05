72d9dcc88c989270aa6c67d0978d7a12fda1ec23
kartiktya <kartiktyagi953@gmail.com>
2024-02-05 20:26:27 +0530

    added view file and helper function util file

---
+++ 5_servingHtmlCssAndJs(VIEWS)/routes/admin.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const rootDir = require('../util.path.js');
+
+const express = require('express');
+
+const router = express.Router();    // creating router object
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+
+    //res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+   
+    //res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;    //router is a object having two routes registered, here exportig router
